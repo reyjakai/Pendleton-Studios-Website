163205546c8f45ab08c8646b9e09ab065d7d659f
Adam Weaver <reyjakai@gmail.com>
2019-07-30 22:14:46 -0600

    Got basic animation working.

--- js/character.js
+++ js/character.js
@@ -1,3 +1,30 @@
+const totalWidth = 384;
+const totalHeight = 768;
+const numRows = 4;
+const numColumns = 4
+
+const initialPosition = (totalWidth / numRows) * 2;
+const positionInterval = totalWidth / numRows;
+
+const initialInterval = 150;
+
+let tID;
+
 function animateScript() {
-    document.getElementById("player").style.backgroundPosition = '-96px 0px';
+    let position = initialPosition;
+    const interval = initialInterval;
+
+    tID = setInterval( () => {
+        document.getElementById("player").style.backgroundPosition = `-${position}px 0px`;
+        if (position < totalWidth) {
+            position += positionInterval;
+        } else {
+            position = positionInterval;
+        }
+    }, initialInterval);
+}
+
+function stopAnimate() {
+    position = initialPosition;
+    clearInterval(tID);
 }
